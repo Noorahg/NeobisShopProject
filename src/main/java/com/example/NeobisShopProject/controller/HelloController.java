d7d772e1fd543fe4a291f59c285a738dcb82c7f8
user <gulnuremilbekova2@gmail.com>
2024-02-13 17:53:22 +0600

    hello

---
+++ src/main/java/com/example/NeobisShopProject/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.NeobisShopProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+
+}
