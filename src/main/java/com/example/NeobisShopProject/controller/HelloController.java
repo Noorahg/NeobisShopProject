1c91c0a0ce42bba25a256148887a2d4050571cdd
user <gulnuremilbekova2@gmail.com>
2024-02-15 01:07:29 +0600

    jwt token register and authentication screenShots

--- src/main/java/com/example/NeobisShopProject/controller/HelloController.java
+++ src/main/java/com/example/NeobisShopProject/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.NeobisShopProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/j")
     public String index() {
         return "Greetings from Spring Boot!";
     }
