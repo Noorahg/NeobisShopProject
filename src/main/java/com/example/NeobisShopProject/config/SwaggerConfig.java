441b3807f86f3901488120f0a8d04ef729505883
user <gulnuremilbekova2@gmail.com>
2024-02-25 02:03:13 +0600

    deploy

--- src/main/java/com/example/NeobisShopProject/config/SwaggerConfig.java
+++ src/main/java/com/example/NeobisShopProject/config/SwaggerConfig.java
@@ -1,18 +1,13 @@
 package com.example.NeobisShopProject.config;
 
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
@@ -20,15 +15,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 @SecurityScheme(type = SecuritySchemeType.HTTP, name = "bearerAuth", scheme = "bearer")
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Value("${DonutShop.openapi.dev-url}")
-    private String shopUrl;
+   // private String shopUrl;
 
     @Bean
     public Docket api() {
@@ -41,11 +36,6 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
 
     private ApiInfo apiInfo() {
-        Contact contact = new Contact();
-        contact.setEmail("gulnuremilbekova2@gmail.com");
-        contact.setName("Gulnur");
-        contact.setUrl("https://github.com/Noorahg");
-
         return new ApiInfoBuilder()
                 .title("Spring Boot Shop API")
                 .version("1.0")
@@ -56,7 +46,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
