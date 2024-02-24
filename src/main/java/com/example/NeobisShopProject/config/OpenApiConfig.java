7cbc2db37d730f75fdb335b548d22a6760401fcf
user <gulnuremilbekova2@gmail.com>
2024-02-25 03:17:07 +0600

    deploy

---
+++ src/main/java/com/example/NeobisShopProject/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+
+package com.example.NeobisShopProject.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+        import io.swagger.v3.oas.annotations.info.Contact;
+        import io.swagger.v3.oas.annotations.info.Info;
+        import io.swagger.v3.oas.annotations.security.SecurityScheme;
+        import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Gulnur",
+                        email = "gulnuremilbekova2@gmail.com"
+                ),
+                title = "Donuts - online  store",
+                description = "OpenApi documentation for  Spring Project",
+                version = "0.0.1"
+        ),
+        servers = {
+                @Server(
+                        description = "Local environment",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenApiConfig {
+}
